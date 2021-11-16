35e2068669bc033ea3cf31808216db38cbc2ae3c
BobanStojanov <boban-stojanov@hotmail.com>
2021-11-16 13:02:30 +0100

    eazy when you know!

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e3",
+    title: "Smoki",
+    amount: 11.12,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Loli Pop",
+    amount: 5.0,
+    date: new Date(2021, 2, 28),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
